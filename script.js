4aff3f547ffc64827f3d0908a043f65320433a3a
gcohen1 <156716839+gcohen1@users.noreply.github.com>
2024-02-28 15:48:57 +0000

    color change

--- script.js
+++ script.js
@@ -9,12 +9,17 @@ function setup() {
 
 
 function draw() {
-  background(88);
+  background(56,0,0);
   textSize(16);
+  fill(255); 
   text("Gracie is so ...", 20, 60);
   textSize(32);
   text("awesome.", 20, 160);
+}
 
-
-
+if(mouseIsPressed){ // true or false?
+  // if true, change the fill color here
+  fill (56,0,0)
+    textSize(16);
+    text("Gracie is so ...", 20, 60);
 }
